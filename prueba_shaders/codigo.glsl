977563d189cd6922c9e581a95e625d9a5b97aa28
SamuelTrusant <55793027+SamuelTrusant@users.noreply.github.com>
2021-04-26 19:58:33 +0100

    modo 8

--- prueba_shaders/codigo.glsl
+++ prueba_shaders/codigo.glsl
@@ -5,23 +5,28 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
+bool right;
+int aux = 0;
+
+//#define t u_Time
+//#define r u_Resolution.xy
 
 void main(){
 	vec3 c;
-    c[0] = 0.056;
-	c[1] += -cos(u_time)/3.;
-	c[2] += sin(u_time)/3.;
+    	c[0] = 0.12;
+	c[1] += -cos(u_time)/2.;
+	c[2] += sin(u_time);
 	float l,z=u_time;
-    float m = mod(u_time,5.);
+    	float m = mod(u_time,5.);
 	for(int i=0;i<3;i++) {
 		vec2 uv,p=gl_FragCoord.xy/u_resolution;
 		uv=p;
-        p.x-=(sin(u_time)/3.)+0.5;
-        p.y-=(cos(u_time)/3.)+0.5;
-
-        z+=0.07;
+        	//p.x-=(sin(u_time)/3.)+0.5;
+        	//p.y-=(cos(u_time)/3.)+0.5;
+        	p.x-=(sin(u_time*2.)/3.)+0.5;
+        	p.y-=(cos(u_time)/3.)+0.5;
 		l=length(p)*4.;
-		uv+=p/l*(sin(z)+2.)*abs(sin(l*9.-z*2.));
+		uv+=p/l*(sin(z)+2.)*abs(sin(l*9.-2.));
         
 		c[i]+=0.01/length(abs(uv-.5));
 	}
